ae09917096df4d229233adb972b8d70b4727a676
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-06-19 19:52:46 +0000

    add postindex layout

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,6 @@
 class Public::PostsController < ApplicationController
   before_action :find_id, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
      # ransackでの検索機能
